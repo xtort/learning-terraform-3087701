964b79663d8fcbb800b1934f80e7e95dd4a72916
Pauly Comtois <jmail@paulycom.com>
2023-03-23 11:14:19 -0700

    test

--- providers.tf
+++ providers.tf
@@ -1,11 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
+      source = "hashicorp/aws"
     }
   }
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region = "us-west-2"
 }
+
+#test line
